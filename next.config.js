c59d84632a3a18113a89a7ab716a46838ff14704
jeongsehun <sehun.dev@gmail.com>
2023-12-14 17:03:40 +0900

    jotai, jest 패키지 추가

--- next.config.js
+++ next.config.js
@@ -2,4 +2,11 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
   openAnalyzer: false,
 })
-module.exports = withBundleAnalyzer({})
+module.exports = withBundleAnalyzer({
+  experimental: {
+    swcPlugins: [
+      ["@swc-jotai/debug-label", {}],
+      ["@swc-jotai/react-refresh", {}],
+    ],
+  },
+})
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next';
 import { Inter as FontSans } from 'next/font/google';
 import { cn } from '@/lib/utils';
+
+import Providers from '@/app/providers';
+
 import './globals.css';
 
 export const fontSans = FontSans({
@@ -17,7 +20,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={cn('min-h-screen bg-background font-sans antialiased', fontSans.variable)}>
-        {children}
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
